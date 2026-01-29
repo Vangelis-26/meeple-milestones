4d364dbc54ee1fe1219b8df69ecd296fe1bb7997
Vangelis-26 <mourier.matthieu@gmail.com>
2026-01-29 23:21:33 +0100

    feat(ChallengeProgress, Dashboard): refactor ChallengeProgress for enhanced ranking display and progress visualization; clean up imports in Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // Ajouté pour la navigation
+import { useNavigate } from 'react-router-dom';
 import { useChallenge } from '../hooks/useChallenge';
 
 // Composants
